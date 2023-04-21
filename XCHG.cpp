34f20926f8ea512bc79fb1d14a0c77bc05232df6
Khushi Naithanigit config --global user.name Khushi <khushinaithani311@gmail.comgit config --global user.email khushinaithani311@gmail.com>
2023-04-22 02:12:57 +0530

    xchg

---
+++ XCHG.cpp
@@ -0,0 +1,12 @@
+#include"header files/master.hpp"
+
+void XCHG(string registers[])
+{
+    string temp;
+    temp=registers[3];
+    registers[3]=registers[5];
+    registers[5]=temp;
+    temp=registers[4];
+    registers[4]=registers[6];
+    registers[6]=temp;
+}
