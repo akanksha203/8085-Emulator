974e0378233ab4259355df2d6bb437300f7225f4
Khushi Naithanigit config --global user.name Khushi <khushinaithani311@gmail.comgit config --global user.email khushinaithani311@gmail.com>
2023-04-22 01:42:19 +0530

    same same

---
+++ opcodeSize.cpp
@@ -0,0 +1,35 @@
+#include "header files/master.hpp"
+
+int opcodeSize(string command)
+{
+    string one[]={"HLT","MOV","DCR","INR","INX","DCX","SUB","ADD","CMA","CMP","XCHG","DAD","STAX"};
+    string two[]={"MVI","ADI","SUB"};
+    string three[]={"STA","SET","LXI","LDA","STA","SHLD","LHLD","JMP","JC","JNZ","JNC","JZ","SET"};
+
+    for(int i=0;i<13;i++)
+    {
+        if(command==one[i])
+            {
+            return 1;
+            }    
+    }
+
+     for(int i=0; i<3; i++)
+    {
+        if(command==two[i])
+            {
+            return 2;
+            }      
+    }
+
+     for(int i=0; i<13; i++)
+    {
+        if(command==three[i])
+            {
+            return 3;
+            }
+               
+    }
+
+    return -1;
+}
