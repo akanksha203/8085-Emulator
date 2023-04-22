a97a9b58870af046fe06fb3adf2b17c8f5ba0b04
Khushi Naithanigit config --global user.name Khushi <khushinaithani311@gmail.comgit config --global user.email khushinaithani311@gmail.com>
2023-04-22 06:11:33 +0530

    to be completed

---
+++ headerFiles/master.hpp
@@ -0,0 +1,15 @@
+#ifndef _MASTER_H
+#define _MASTER_H
+
+#include<iostream>
+#include<math.h>
+#include<string>
+#include<vector>
+#include<fstream>
+#include<map>
+#include <algorithm>
+#include<array>
+using namespace std;
+
+
+#endif
